495fc8aa0b447427cf9f4f51cefd6d91fe718d75
Natniif <fintan.hardy@gmail.com>
2024-05-03 18:15:32 +0100

    changes to template

--- problems/template.cpp
+++ problems/template.cpp
@@ -2,22 +2,42 @@
 
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
-
-typedef vector<int> vi;
-typedef vector<ll> vl;
+using lli = long long int;
+using uInt = unsigned int;
+using ulli = unsigned long long int;
+using vi = vector<int>;
+using vii = vector<lli>;
+using vc = vector<char>;
+using udouble = long double;
+using vd = vector<double>;
+using vs = vector<string>;
 
 #define FOR(i, a, b) for(int i=a; i<(b)l i++)
-#define F0R(i, a) for (int i=0l i<(a); i++)
-#define trac(a, x) for (auto& a : x)
+#define F0R(i, a) for(int i=0l i<(a); i++)
+#define trac(a, x) for(auto& a : x)
 #define uid(a, b) uniform_int_distribution(a, b)(rng)
 
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define lb lower_bound
-#define up upper_bound
+
+#define ln                "\n"
+#define flush             endl
+#define sz(x)             (int)(x).size()
+
+#define bin_sc(a, x)      binary_search(All(a), x)
+
+#define scanv(x)          for(auto &i: x) cin >> i
+#define msg(x)            cout << (#x) << ln
+#define printv(v)         cout << "[ ";for(auto j: v) cout << j << ' '; cout << "]" << ln
+#define printvv(v)        cout << "["; for(auto i: v) {printv(i);} cout << "........]\n"
+
+#define mp                make_pair
+#define pb                push_back
+#define lb                lower_bound
+#define up                upper_bound
+
+bool isUp(char ch){locale loc; return isupper(ch, loc);}
+void debug(vs s) {cout << "#------------------#\n";}
+
+template<typename T> T LOG(T base, T power){return log2(power) / log2(base);}
 
 void solve() {
 
