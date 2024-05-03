72f3c11321b703a9e2e5964e77069e99d1645645
Natniif <fintan.hardy@gmail.com>
2024-05-03 17:27:39 +0100

    first problem plus a tonne of setup :)

---
+++ new_problem.py
@@ -0,0 +1,38 @@
+import os, sys
+
+def add_include(filename): 
+    
+    new_file = os.path.join("./problems", filename)
+
+    if not os.path.exists(new_file): 
+        os.makedirs(new_file)
+
+    filepath = os.path.join(new_file, filename + ".cpp")
+
+    if os.path.exists(filepath): 
+        print("File already exists")
+        return
+
+    if not os.path.exists("./problems/template.cpp"):
+        print("Template folder not found")
+        return
+
+    with open("./problems/template.cpp", "r") as f:
+        contents = f.read()
+
+    with open(filepath, "w") as f: 
+        f.write(contents)
+        
+
+    print(f"Template folder created for problem {filename} in ./problems directory")
+
+
+if __name__ == "__main__": 
+    if len(sys.argv) != 2: 
+        print("Usage: new_problem.py [problem number]")
+    
+    else: 
+        add_include(sys.argv[1])
+
+
+
---
+++ problems/template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+#define FOR(i, a, b) for(int i=a; i<(b)l i++)
+#define F0R(i, a) for (int i=0l i<(a); i++)
+#define trac(a, x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution(a, b)(rng)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define up upper_bound
+
+
+
+
+void solve() {
+
+} 
+
+int main() {
+    
+    solve();
+
+}
