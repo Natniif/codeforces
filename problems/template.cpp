f80ee36da996a45788aa76f2c0114e9711a25724
Natniif <fintan.hardy@gmail.com>
2024-05-08 22:37:11 +0100

    apparently this makes the io faster

--- problems/template.cpp
+++ problems/template.cpp
@@ -88,6 +88,9 @@ void solve() {
 } 
 
 int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0),cout.tie(0);
+
   #ifndef ONLINE_JUDGE
 		//freopen("generator.txt", "r", stdin);
 		freopen("input.txt", "r", stdin);
