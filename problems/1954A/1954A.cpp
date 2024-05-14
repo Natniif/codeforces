eb6a14d74258a892a796c6ad22f164ff13b4fb7f
Natniif <fintan.hardy@gmail.com>
2024-05-14 19:06:09 +0100

    came back and destroyed question oh hell yeah

--- problems/1954A/1954A.cpp
+++ problems/1954A/1954A.cpp
@@ -2,22 +2,87 @@
 
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
+using lli = long long int;
+using uInt = unsigned int;
+using ulli = unsigned long long int;
+using vi = vector<int>;
+using vii = vector<lli>;
+using vc = vector<char>;
+using udouble = long double;
+using vd = vector<double>;
+using vs = vector<string>;
 
-typedef vector<int> vi;
-typedef vector<ll> vl;
-
-#define FOR(i, a, b) for(int i=a; i<(b)l i++)
-#define F0R(i, a) for (int i=0l i<(a); i++)
-#define trac(a, x) for (auto& a : x)
+#define FOR(i, a, b) for(int i=a; i<(b); i++)
+#define F0R(i, a) for(int i=0l i<(a); i++)
+#define trac(a, x) for(auto& a : x)
 #define uid(a, b) uniform_int_distribution(a, b)(rng)
 
-#define sz(x) (int)(x).size()
-#define mp make_pair
-#define pb push_back
-#define lb lower_bound
-#define up upper_bound
+
+#define ln                "\n"
+#define flush             endl
+#define sz(x)             (int)(x).size()
+
+#define bin_sc(a, x)      binary_search(All(a), x)
+
+#define scanv(x)          for(auto &i: x) cin >> i
+#define msg(x)            cout << (#x) << ln
+#define printv(v)         cout << "[ ";for(auto j: v) cout << j << ' '; cout << "]" << ln
+#define printvv(v)        cout << "["; for(auto i: v) {printv(i);} cout << "........]\n"
+
+#define mp                make_pair
+#define pb                push_back
+#define lb                lower_bound
+#define up                upper_bound
+
+bool isUp(char ch){locale loc; return isupper(ch, loc);}
+void debug() {cout << "#------------------#\n";}
+
+template<typename T> T LOG(T base, T power){return log2(power) / log2(base);}
+
+//--------- overloaded << operator to print stl containers.. cout << container << ln
+// for vector(1D, 2D), vector of pair & set....
+template<typename T>
+ostream& operator<< (ostream& out, const vector<T>& v) {
+    out << "[";
+    size_t last = v.size() - 1;
+    for (size_t i = 0; i < v.size(); ++i) {
+        out << v[i];
+        if (i != last)
+            out << ", ";
+    }
+    out << "]";
+    return out;
+}
+
+// for set & set of ....
+template <typename T> 
+ostream& operator<<(ostream& os, const set<T>& v){ 
+    os << "{"; 
+    for (auto it : v) { 
+        os << it; 
+        if (it != *v.rbegin()) 
+            os << ", "; 
+    } 
+    os << "}"; 
+    return os; 
+} 
+// for map, map of vector & .....
+template <typename T, typename S> 
+ostream& operator<<(ostream& os, const map<T, S>& v){ 
+    for (auto it : v)  
+        os << it.first << " : " 
+           << it.second << "\n"; 
+    return os; 
+}
+// for pair & pair of .......
+template <typename T, typename S> 
+ostream& operator<<(ostream& os, const pair<T, S>& v){ 
+    os << "("; 
+    os << v.first << ", " 
+       << v.second << ")"; 
+    return os; 
+}
+
 
 void solve() {
   int n, m, k; cin >> n >> m >> k;
@@ -27,12 +92,24 @@ void solve() {
  // k is how many parts can be destroyed 
   
   // if name colours -1 is not bigger than num of deletions then fail
-  if (k >= n - 1) {
-    cout << "YES";
+  int repeat_min = floor((float)n/(float)m);
+
+  int add = n%m;
+
+  // all except one colour need to be eliminated
+  if (add == 0) {
+    if (k < (m - 1)*repeat_min) {
+      cout << "YES" << ln;
+      return;
+    }
   } else {
-  
-    cout << (m > 1 ? "YES" : "NO");
+    if (k < ((m - 1)*repeat_min) + add - 1) {
+      cout << "YES" << ln;
+      return;
+    }
   }
+
+  cout << "NO" << ln;
   
 
 } 
